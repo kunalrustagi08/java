e978de35622d7594e6748d76d432565a5a5b6020
Kunal Rustagi <kunal.rustagi@gmail.com>
2021-09-05 17:53:15 +0530

    Day 0

---
+++ 01_edX_java_basics/SumOf2Numbers.java
@@ -0,0 +1,8 @@
+public class SumOf2Numbers {
+    public static void main(String[] args) {
+        int a = 10, b = 10;
+        int sum = 0;
+        sum = a +b;
+        System.out.println("Sum is" + sum);
+    }
+}
